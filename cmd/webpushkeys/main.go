97b5b581900f9827e2804ac52d0a99408c447015
Derek Battams <derek@battams.ca>
2022-12-18 17:00:48 -0500

    apply license to src files

--- cmd/webpushkeys/main.go
+++ cmd/webpushkeys/main.go
@@ -1,6 +1,25 @@
 package main
 
 /*
+	mstdnlambda
+	Copyright (C) 2022 Battams, Derek <derek@battams.ca>
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License along
+	with this program; if not, write to the Free Software Foundation, Inc.,
+	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+/*
 	The webpushkeys command is a simple command line utility that will generate
 	a unique set of keys and secrets needed to successfully subscribe an endpoint
 	to Mastodon push notifications.
